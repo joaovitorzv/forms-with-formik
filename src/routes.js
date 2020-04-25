18dab256d46afeff316ed7b371d4f868df6fa820
joao <resetgamer1@gmail.com>
2020-04-25 12:44:37 -0300

    finished

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/register' component={Register} />
+        <Route path='/' component={Register} />
         <Route path='/login' component={Login} />
       </Switch>
     </BrowserRouter>
